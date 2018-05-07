c350b6ee068d789edb5f7f95e9e64b50ebb1d2f5
oliverlai55 <oliverlai55@gmail.com>
2018-05-07 00:51:02 -0400

    Add constructore notes

--- oop/person.js
+++ oop/person.js
@@ -1,10 +1,17 @@
-const person = function(firstName) {
+const Person = function(firstName, lastName, age) {
 	this.firstName = firstName;
+	this.lastName = lastName;
+	this.age = age;
 };
 
-const me = new person('Oliver');
+const me = new Person('Oliver', 'Mead', 27);
 
 console.log(me);
 
 // function that use new are constructors
-// new operator generates a new empty object for the new instance, and give access to empty object in constructor fucntion via the 'this' value
+// new operator generates a new empty object for the new instance, and give access to empty object in constructor function via the 'this' value
+// new operator creates a new object and uses it as return value implicitly
+// still use es5 function because arrow functions don't bind 'this'
+
+const person2 = new Person('Tom', 'Smith', 20);
+console.log(person2);
